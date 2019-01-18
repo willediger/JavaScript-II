7c81074f592e950d0e4c7e624206494aaf0d5c2b
will Ediger <will.ediger@gmail.com>
2019-01-18 00:03:27 -0600

    closure - challenges 1 & 2

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,32 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+const foo = 'bar';
+const closure = () => foo;
+
+console.log(closure());
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  count++;
+  return count;
 };
+
+let count = 0;
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+
+const newCounter = counter;
+console.log(count); // 0
+
+newCounter(); // 1
+console.log(count);
+
+newCounter(); // 2
+console.log(count);
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
