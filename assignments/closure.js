ffe45975020f87a27841b28616c868a5661861d1
will Ediger <will.ediger@gmail.com>
2019-01-18 00:33:55 -0600

    added missing semicolons in closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -12,7 +12,7 @@ const counter = () => {
   return () => {
     count++;
     return count;
-  }
+  };
 };
 
 let count = 0;
@@ -32,7 +32,7 @@ console.log(count);
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
-console.log('*****stretch*****')
+console.log('*****stretch*****');
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
@@ -45,7 +45,7 @@ const counterFactory = () => {
       count2--;
       return count2;
     }
-  }
+  };
   return incrDecr;
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
