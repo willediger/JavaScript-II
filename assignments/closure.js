cf10d3962c53ae8ca1bef7646798c3fa47d0683d
will Ediger <will.ediger@gmail.com>
2019-01-18 00:13:34 -0600

    closure stretch

--- assignments/closure.js
+++ assignments/closure.js
@@ -30,9 +30,36 @@ console.log(count);
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
+console.log('*****stretch*****')
+
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
+  const incrDecr = {
+    increment: () => {
+      count2++;
+      return count2;
+    },
+    decrement: () => {
+      count2--;
+      return count2;
+    }
+  }
+  return incrDecr;
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
 };
+
+let count2 = 0;
+console.log(count2); // 0
+
+const incrDecrInstance = counterFactory();
+
+incrDecrInstance.increment();
+console.log(count2); // 1
+
+incrDecrInstance.increment();
+console.log(count2); // 2
+
+incrDecrInstance.decrement();
+console.log(count2); // 1
